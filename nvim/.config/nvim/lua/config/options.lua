99757b827c6d89cebe388d44a47b87890f85d69d
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-09-27 13:02:14 +0700

    feat: nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -16,4 +16,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.bo.expandtab = true
   end,
 })
-
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,4 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      pyright = {
-        settings = {
-          python = {
-            pythonPath = vim.fn.expand("/home/paultsai/miniconda3/envs/AI/bin/python"),
-          },
-        },
-      },
-    },
-  },
+  opts = {},
 }
