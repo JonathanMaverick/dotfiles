5842ffdd7d094eebbc01b4f335f80e57456de1dd
Jonathan Maverick <jonathanmaverick.jm@gmail.com>
2025-11-01 11:18:24 +0700

    feat: show .env file Thank you Niel <3

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      ignored = true,
+      files = {
+        hidden = true,
+      },
+    },
+    explorer = {
+      ignored = true,
+      files = {
+        hidden = true,
+      },
+    },
+  },
+}
